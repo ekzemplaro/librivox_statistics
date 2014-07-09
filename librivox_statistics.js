3db74cb70b138dc76f218e789118e1b483b6a1ce
ekzemplaro <linux@hi-ho.ne.jp>
2014-07-09 20:15:23 +0900

    commit Jul/09/2014

--- librivox_statistics.js
+++ librivox_statistics.js
@@ -1,17 +1,17 @@
 // -----------------------------------------------------------------------
 //	librivox_statistics.js
 //
-//					Jun/02/2014
+//					Jul/02/2014
 //
 // -----------------------------------------------------------------------
 jQuery (function ()
 {
 	jQuery("#outarea_aa").html
-		("*** librivox_statistics *** start *** Jun/02/2014 ***");
+		("*** librivox_statistics *** start *** Jul/02/2014 ***");
 
 	var file_json = "latest_on_going.json";
 
-	var text_months = gen_month_button_row_proc (2014,6);
+	var text_months = gen_month_button_row_proc (2014,7);
 	text_months += gen_month_button_row_proc (2013,12);
 	text_months += gen_month_button_row_proc (2012,12);
 	text_months += gen_month_button_row_proc (2011,12);
@@ -25,7 +25,7 @@ jQuery (function ()
 	fetch_json_proc (text_months,file_json);
 
 	jQuery("#outarea_hh").html
-		("*** librivox_statistics *** end *** Jun/02/2014 ***");
+		("*** librivox_statistics *** end *** Jul/02/2014 ***");
 });
 
 // -----------------------------------------------------------------------
