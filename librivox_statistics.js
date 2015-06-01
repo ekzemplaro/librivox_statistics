fc967909848154c210cfd6f0ae340562f48fce3f
uchida <linux@hi-ho.ne.jp>
2015-06-01 20:51:37 +0900

    Commit on Jun/01/2015

--- librivox_statistics.js
+++ librivox_statistics.js
@@ -1,17 +1,17 @@
 // -----------------------------------------------------------------------
 //	librivox_statistics.js
 //
-//					May/01/2015
+//					Jun/01/2015
 //
 // -----------------------------------------------------------------------
 jQuery (function ()
 {
 	jQuery("#outarea_aa").html
-		("*** librivox_statistics *** start *** May/01/2015 ***");
+		("*** librivox_statistics *** start *** Jun/01/2015 ***");
 
 	var file_json = "latest_on_going.json";
 
-	var text_months = gen_month_button_row_proc (2015,5);
+	var text_months = gen_month_button_row_proc (2015,6);
 	text_months += gen_month_button_row_proc (2014,12);
 	text_months += gen_month_button_row_proc (2013,12);
 	text_months += gen_month_button_row_proc (2012,12);
@@ -26,7 +26,7 @@ jQuery (function ()
 	fetch_json_proc (text_months,file_json);
 
 	jQuery("#outarea_hh").html
-		("*** librivox_statistics *** end *** May/01/2015 ***");
+		("*** librivox_statistics *** end *** Jun/01/2015 ***");
 });
 
 // -----------------------------------------------------------------------
